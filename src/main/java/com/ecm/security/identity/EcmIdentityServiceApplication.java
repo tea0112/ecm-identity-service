3cb98fba683b6126d6b7535f2f896bc51b2ba083
tea <ducthaidev@gmail.com>
2025-09-29 10:03:40 +0700

    first commit

---
+++ src/main/java/com/ecm/security/identity/EcmIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecm.security.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmIdentityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecm/security/identity/EcmIdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecm.security.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
